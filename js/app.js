1faf6abb6efb3eef3410f079e3326d90411ded13
Omar Mohamed <omarlawatey@gmail.com>
2022-07-07 22:24:20 +0200

    style: fix nav menu animation

--- js/app.js
+++ js/app.js
@@ -1,16 +1,13 @@
-const controlDropdown = () => ({
-  on: innerWidth > 1000 ? 'hover' : 'click'
-});
-
-const dropdowns = $('.ui.dropdown');
-dropdowns.dropdown(controlDropdown());
-
-onresize = () => dropdowns.dropdown(controlDropdown());
-
 const navToggler = document.querySelector('.humburger');
 const nav = document.querySelector('nav');
 
-let isOpen = false;
+const dropdown = $('.ui.dropdown');
+const controlDropdown = () => ({
+  on: innerWidth > 1000 ? 'hover' : 'click'
+});
+dropdown.dropdown(controlDropdown());
+onresize = () => dropdown.dropdown(controlDropdown());
+
 navToggler.addEventListener('click', () => {
   nav.classList.toggle('open');
 });
